9e13008ea86e94aab16192980b5b3829d7d26b1d
zacharyfmarion <zachary.marion@duke.edu>
2015-08-04 10:36:32 -0400

    working paper-select element

--- gulpfile.js
+++ gulpfile.js
@@ -226,7 +226,7 @@ gulp.task('serve:dist', ['default'], function () {
     // Run as an https by uncommenting 'https: true'
     // Note: this uses an unsigned certificate which on first access
     //       will present a certificate warning in the browser.
-    // https: true,
+    https: true,
     server: 'dist',
     middleware: [ historyApiFallback() ]
   });
